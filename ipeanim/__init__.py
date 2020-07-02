f2a2ba30814c4a258705864c9ee22b9d7b484940
soerface <soerface@users.noreply.github.com>
2020-07-02 23:50:10 +0000

    Initialized the repository

---
+++ ipeanim/__init__.py
@@ -0,0 +1,6 @@
+from pbr.version import VersionInfo
+
+__version__ = VersionInfo('ipeanim').version_string()
+__version_info__ = VersionInfo('ipeanim').semantic_version().version_tuple()
+
+from .hello import HelloWorld
